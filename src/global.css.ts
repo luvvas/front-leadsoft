635eb54d5ca776c7eb4e0c5cae58fe6ec2eaf1aa
luvvas <lucasmachado1@ufpr.br>
2022-11-28 18:37:14 -0300

    adding globalcss style

--- src/global.css.ts
+++ src/global.css.ts
@@ -1,7 +1,13 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+  :root {
+    font-size: 1rem;
+    font-weight: 400;
+  }
+
   * {
+    font-family: 'Roboto';
     margin: 0;
     padding: 0;
     outline: 0;
@@ -19,5 +25,10 @@ export default createGlobalStyle`
   body {
     min-width: 320px;
     min-height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #5772f7;
   }
 `
