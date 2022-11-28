01af4eb937d1d9b8835e1e0f19f770b84ed84095
luvvas <lucasmachado1@ufpr.br>
2022-11-28 18:43:52 -0300

    adding route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Teste</h1>
-    </div>
-  );
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import GlobalCSS from "./global.css";
+
+import { AuthContext, useAuthContext } from "./contexts/Auth";
+import { Login } from "./pages/Login";
+
+interface AuthenticationProps {
+  isAuth: boolean;
 }
 
-export default App;
+const AuthRoute = ({ isAuth }: AuthenticationProps) =>
+  isAuth ? <Outlet /> : <Navigate to="/login" replace />;
+
+export default function App() {
+  const { isAuth } = useAuthContext();
+
+  return (
+    <>
+      <GlobalCSS />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<AuthRoute isAuth={isAuth} />}>
+          <Route path="/home" element={<h1>HOME</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/home" />} />
+      </Routes>
+      <ToastContainer style={{ width: "30px", height: "30px" }} />
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import GlobalCSS from "./global.css";
+
+import { AuthContext } from "./contexts/Auth";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <GlobalCSS />
-    <App />
+    <BrowserRouter>
+      <AuthContext>
+        <App />
+      </AuthContext>
+    </BrowserRouter>
   </React.StrictMode>
 );
