990e6c7d354112587872d7247d5fea27b6d0c43a
luvvas <lucasmachado1@ufpr.br>
2022-11-28 18:42:27 -0300

    adding Input style

---
+++ src/components/Input/style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const InputStyle = styled.div`
+  input {
+    width: 100%;
+    font-size: 16px;
+    padding-bottom: 10px;
+    line-height: 1.2;
+    border: none;
+    background-color: transparent;
+    border-bottom: 2px solid #adadad;
+  }
+
+  span {
+    display: block;
+    margin-top: 10px;
+    color: red;
+    font-size: 0.675rem;
+  }
+`;
