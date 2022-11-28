77882e243f040cad68eb60c0927faaaf13589227
luvvas <lucasmachado1@ufpr.br>
2022-11-28 18:40:37 -0300

    adding button style

---
+++ src/components/Button/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ButtonStyle = styled.button`
+  background-color: #3d69fc;
+  border-radius: 8px;
+  border: 1px solid;
+  color: #FFF;
+  cursor: pointer;
+
+  padding: 10px 1em;
+  text-align: center;
+  text-decoration: none;
+
+  &:hover,
+  &:focus {
+    background-color: #182e85;
+  }
+`;
